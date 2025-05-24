e51f511b4b01c8a30e0a57c2218563d588b5bc08
Swarangii <swarangiyadav557@gmail.com>
2025-05-24 08:36:33 +0530

    Variables Done

---
+++ Basics/variables.js
@@ -0,0 +1,21 @@
+// ? Let - Var - Const
+
+const accId = 123456;
+let accEmail = "swarangiyadav557@gmail.com";
+var accPass = "2345";
+accCity = "GWL";
+
+let accState;
+
+// ? changing
+// accId = 123;
+accEmail = "swara@gmail.com";
+accPass = "7846";
+accCity = "BHL";
+
+console.log(accId);
+console.table([accId, accEmail, accPass, accCity, accState]);
+
+/*
+No to var because of issue in block scope and functional scope
+ */
