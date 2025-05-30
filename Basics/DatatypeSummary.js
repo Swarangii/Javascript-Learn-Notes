38c5058f8b7bcaf84465cb0c8da07cafbc4b47fb
Swarangii <swarangiyadav557@gmail.com>
2025-05-30 18:07:35 +0530

    Study of Datatypes - Primitive / non-Primitive

--- Basics/DatatypeSummary.js
+++ Basics/DatatypeSummary.js
@@ -18,3 +18,18 @@ console.log(typeof bigNumber);
 // Referensive (non-Primitive) datatype
 
 // array, object, functions
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
