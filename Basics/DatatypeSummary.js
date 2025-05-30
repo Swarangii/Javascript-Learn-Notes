2718e34594f05a89f6907a5f9f1d3ae5ecd7e6d4
Swarangii <swarangiyadav557@gmail.com>
2025-05-30 18:06:00 +0530

    Breif study of Data Types

---
+++ Basics/DatatypeSummary.js
@@ -0,0 +1,20 @@
+// #Primitive Datatype
+
+// 7 Types: string, number, boolean, null, undefined, Symbol, bigInt
+
+const score = 100;
+const scoreValue = 100.5;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherID = Symbol("123");
+console.log(id === anotherID);
+
+const bigNumber = 123456789n;
+console.log(typeof bigNumber);
+
+// Referensive (non-Primitive) datatype
+
+// array, object, functions
