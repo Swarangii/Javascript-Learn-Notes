4c8ef4ec5b180234d1699cb09bf67d9454d72a25
Swarangii <swarangiyadav557@gmail.com>
2025-05-31 12:04:13 +0530

    Numbers and Math

--- Basics/07_numbersAndMath.js
+++ Basics/07_numbersAndMath.js
@@ -1,6 +1,6 @@
 // Numbers and math
 
-// *Numbers
+// * Numbers
 
 const score = 100;
 console.log(score);
@@ -31,3 +31,19 @@ console.log(hundreds.toLocaleString("en-IN"));
 // .toPrecision(3) = 23.8904 =>23.9
 // .toLocaleString() = 1000000=>1,000,000
 // .toLocaleString('en-IN') = 1000000=>10,00,000
+
+// * Math
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 5, 6, 7));
+console.log(Math.max(4, 5, 6, 7));
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.random() * (max - min + 1) + min);
