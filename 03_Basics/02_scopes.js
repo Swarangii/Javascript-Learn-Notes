31a045d7c43624617e645ddc13443a6b03777316
Swarangii <swarangiyadav557@gmail.com>
2025-06-09 23:52:06 +0530

    scope level and mini hoisting

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -11,3 +11,42 @@ if (true) {
 console.log(a);
 // console.log(b);
 console.log(c);
+
+// Nested Scope --
+
+function one() {
+  const username = "Swarangi";
+  function two() {
+    const website = "github";
+    console.log(username);
+  }
+  // console.log(website);
+  two();
+}
+one();
+
+if (true) {
+  const username = "Swarangi";
+  if (username === "Swarangi") {
+    const website = " github";
+    console.log(username + website);
+  }
+  // console.log(website);
+}
+
+// console.log(username);
+
+// ++++++ interesting ++++++
+
+console.log(addone(5));
+
+function addone(num) {
+  return num + 1;
+}
+
+// we can't place a function call before function initialized, created through const!!
+const addtwo = function (num) {
+  return num + 2;
+};
+
+addtwo(5);
