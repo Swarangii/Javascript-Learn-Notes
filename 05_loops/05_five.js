e44fa8da963d2e9d4fc4ff17e3a164675462d3b7
Swarangii <swarangiyadav557@gmail.com>
2025-06-24 20:25:49 +0530

    map function

--- 05_loops/05_five.js
+++ 05_loops/05_five.js
@@ -1,7 +1,7 @@
 // const coding = ["js", "ruby", "java", "python", "cpp"]
 
 // const values = coding.forEach( (item) => {
-//     //console.log(item);
+//console.log(item);
 //     return item
 // } )
 
